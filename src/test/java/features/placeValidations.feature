f566f4bdffa77bc5b8faaf78c53139961ae081d4
Administrator <Administrator@WINDOWS-7BKQF2L>
2026-02-17 19:04:43 +0530

    cucumber feature implemented 17/02/2026

--- src/test/java/features/placeValidations.feature
+++ src/test/java/features/placeValidations.feature
@@ -3,8 +3,8 @@ Feature: Validating Place API's
 Scenario: Verify if place is being Successfully added using AddPlaceAPI
          Given Add Place Payload
          When user calls "AddPlaceAPI" with Post http request
-         Then the API call is success with status code 200
-         And "status" is response body is "Ok"
+         Then the API call got success with status code 200
+         And "status" in response body is "OK"
          And "scope" in response body is "APP"
          
 
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,7 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -20,13 +22,16 @@ import pojo.Location;
 
 public class StepDefinition {
 	
-	@Given("Add Place Payload")
-	public void add_Place_Payload() {
-		
-	RestAssured.baseURI="https://rahulshettyacademy.com";
-		
-	RestAssured.baseURI="https://rahulshettyacademy.com";
+	RequestSpecification res;
+    Response response;
+    ResponseSpecification resspec;
+    RequestSpecification req;
 	
+	@Given("Add Place Payload")
+	public void add_place_payload() {
+		
+	RestAssured.baseURI="https://rahulshettyacademy.com";
+			
 	AddPlace p=new AddPlace();
 	p.setAccuracy(50);
 	p.setAddress("29, side layout, cohen 09");
@@ -44,29 +49,42 @@ public class StepDefinition {
 	loc.setLng(33.427362);
 	p.setLocation(loc);
 	
-	RequestSpecification req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
+	 req=new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
 	.setContentType(ContentType.JSON).build();
 	
 	
 	
-	ResponseSpecification resspec=new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+	 resspec=new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 	
-	RequestSpecification res=given().spec(req)
+	 res=given().log().all().spec(req)
 	.body(p);
 	}
-	@When("user calls {String} with Post http request")
+	@When("user calls {string} with Post http request")
 	public void user_calls_with_Post_http_request(String string) {
 		
-		Response response=res.when().post("/maps/api/place/add/json")
-				.then().spec(resspec).extract().response();
+		 response=res.when().post("/maps/api/place/add/json")
+				 .then().log().all().spec(resspec).extract().response();
+				
 
 	}
 	
-	@Then("the API call is success with status code {int}")
-	public void the_API_call_got_success_with_status_code(Integer int1) {
+	@Then("the API call got success with status code {int}")
+	public void the_api_call_got_success_with_status_code(Integer int1) {
 		
-		
+		assertEquals(response.getStatusCode(), 200);
 
 	}
+	
+	@Then("{string} in response body is {string}")
+	public void the_response_body_is(String keyValue , String Expectedvalue) {
+		String resp=response.asString();
+		JsonPath js=new JsonPath(resp);
+		assertEquals(js.get(keyValue).toString(), Expectedvalue);
+		
+		
+		
+			}
+	
+	
 
 }
